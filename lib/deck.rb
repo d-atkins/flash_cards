5e784761965b241a11f0999128b481f467142c9b
David Atkins <atki1080@gmail.com>
2019-11-19 08:55:42 -0700

    Fix cards_in_category method to not mutate @cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,7 +7,9 @@ class Deck
   end
 
   def cards_in_category(category)
-    new_cards = cards.keep_if{|card| card.category == category}
+    new_cards = Array.new
+    new_cards.replace(cards)
+    new_cards.keep_if{|card| card.category == category}
   end
 
 end
