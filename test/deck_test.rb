247838cb6707041ceb16c734d4268d36710a2593
David Atkins <atki1080@gmail.com>
2019-11-19 08:38:32 -0700

    Fix test_cards_in_category to coincide with deck.rb fix

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -42,7 +42,7 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    assert_equal 2, deck.cards_in_category(:STEM)
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
   end
 
 end
