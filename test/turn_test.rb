9b2d3e4cd47107890193a4c36346fe21a23001e7
David Atkins <atki1080@gmail.com>
2019-11-18 23:24:57 -0700

    Add correct? method test cases

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -27,16 +27,23 @@ class TurnTest < Minitest::Test
     assert_instance_of Card, turn.card
   end
 
+
+
+  def test_correct_is_true
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal true, turn.correct?
+  end
+
+  def test_correct_is_false
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal false, turn.correct?
+  end
+
 =begin
-
-  def test_correct
-
-    assert_equal
-  end
-
-  def test_incorrect
-    assert_equal
-  end
 
   def test_correct_feedback
     assert_equal
@@ -45,6 +52,6 @@ class TurnTest < Minitest::Test
   def test_incorrect_feedback
     assert_equal
   end
-  
+
 =end
 end
