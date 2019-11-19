ca0ab8e868b424710c2fe6bc4a2dd2cd4c8cec03
David Atkins <atki1080@gmail.com>
2019-11-18 23:14:47 -0700

    Add test_it_exists test case

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,7 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/turn'
+require '../lib/card'
+
 
 class TurnTest < Minitest::Test
 
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Saturn", card)
+  end
 end
