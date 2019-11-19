9adf71728c8a3b9306121df9475cfbbd9c396081
David Atkins <atki1080@gmail.com>
2019-11-18 23:22:25 -0700

    Add class attribute test cases

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,7 +9,42 @@ class TurnTest < Minitest::Test
   def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Saturn", card)
+
     assert_instance_of Turn, turn
   end
 
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal "Saturn", turn.guess
+  end
+
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Saturn", card)
+
+    assert_instance_of Card, turn.card
+  end
+
+=begin
+
+  def test_correct
+
+    assert_equal
+  end
+
+  def test_incorrect
+    assert_equal
+  end
+
+  def test_correct_feedback
+    assert_equal
+  end
+
+  def test_incorrect_feedback
+    assert_equal
+  end
+  
+=end
 end
