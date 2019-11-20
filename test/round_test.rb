5dfb702d14ca4a7ac6eda242b2ea348c3ae42c72
David Atkins <atki1080@gmail.com>
2019-11-19 21:19:19 -0700

    Add test_it_exists test case

--- test/round_test.rb
+++ test/round_test.rb
@@ -4,6 +4,12 @@ require './lib/deck'
 require './lib/card'
 require './lib/round'
 
+class RoundTest < Minitest::Test
 
+  def test_it_exists
+    round = Round.new
+
+    assert_instance_of Round, round
+  end
 
 end
