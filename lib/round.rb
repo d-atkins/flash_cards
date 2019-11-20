b1d5a0755955aa5ed2d4d92dcc15445391031e2f
David Atkins <atki1080@gmail.com>
2019-11-19 22:09:44 -0700

    Add current_card to initialize method

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,30 @@
+require './lib/turn'
+require './lib/deck'
 
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :current_card
 
   def initialize (deck)
     @deck = deck
+    @current_card = deck.cards[0]
   end
+
+  # def take_turn (guess)
+  #   get first card in deck
+  #   use guess and card to create a turn object
+  # end
+  #
+  # def number_correct
+  # end
+  #
+  # def correct_by_category
+  # end
+  #
+  # def percent_correct
+  # end
+  #
+  # def percent_correct_by_category
+  # end
+
 
 end
