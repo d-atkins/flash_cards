ca0966e26388b163ee385610881cc1015394c029
David Atkins <atki1080@gmail.com>
2019-11-20 07:28:05 -0700

    Add number_correct counter in take_turn

--- lib/round.rb
+++ lib/round.rb
@@ -2,23 +2,27 @@ require './lib/turn'
 require './lib/deck'
 
 class Round
-  attr_reader :deck, :turns, :current_card
+  attr_reader :deck, :turns, :current_card, :number_correct
 
   def initialize (deck)
     @deck = deck
     @current_card = deck.cards[0]
     @turns = []
+    @number_correct = 0
   end
 
   def take_turn (guess)
     turn = Turn.new(guess, @current_card)
     @turns << turn
+
+    if turn.correct?
+      @number_correct += 1
+    end
+
     @current_card = deck.cards[@deck.cards.index(@current_card) + 1]
     return turn
   end
   #
-  # def number_correct
-  # end
   #
   # def correct_by_category
   # end
