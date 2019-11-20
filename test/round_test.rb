eca986098705702398afdad827161dc6312749e4
David Atkins <atki1080@gmail.com>
2019-11-20 08:54:44 -0700

    Add test_percent_correct test case

--- test/round_test.rb
+++ test/round_test.rb
@@ -110,8 +110,9 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     round.take_turn("Juneau")
     round.take_turn("Venus")
+    count = round.turns.count
 
-    assert_equal 2, round.turns.count
+    assert_equal 2, count
   end
 
   def test_last_turn_feedback
@@ -123,8 +124,9 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     round.take_turn("Juneau")
     round.take_turn("Venus")
+    feedback = round.turns.last.feedback
 
-    assert_equal "Incorrect.", round.turns.last.feedback
+    assert_equal "Incorrect.", feedback
   end
 
   def test_number_correct_by_category
@@ -136,9 +138,36 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     round.take_turn("Juneau")
     round.take_turn("Venus")
+    category_correct = round.number_correct_by_category(:Geography)
 
-    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 1, category_correct
   end
+
+  def test_percent_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 50.0, round.percent_correct
+  end
+  #
+  # def test_percent_correct_by_category
+  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #   cards = [card_1, card_2, card_3]
+  #   deck = Deck.new(cards)
+  #   round = Round.new(deck)
+  #   round.take_turn("Juneau")
+  #   round.take_turn("Venus")
+  #
+  #   assert_equal 100.0, round.percent_correct_by_category(:Stem)
+  # end
 
 
 end
