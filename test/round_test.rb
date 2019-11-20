44d759654eb8a0af295f422f978f89eae18ebcf2
David Atkins <atki1080@gmail.com>
2019-11-19 22:09:09 -0700

    Add test_initial_current_card test case

--- test/round_test.rb
+++ test/round_test.rb
@@ -27,12 +27,20 @@ class RoundTest < Minitest::Test
 
     assert_instance_of Deck, round.deck
   end
-  
+
   #
   # def test_it_has_turns
   # end
   #
-  # def test_current_card
-  # end
+  def test_initial_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
 
 end
