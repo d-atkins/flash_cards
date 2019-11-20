0fa21ed3564ed16e8a6d30684c0c4ff17f4be295
David Atkins <atki1080@gmail.com>
2019-11-19 22:34:29 -0700

    Add test_current_card_changes_to_next_card test case

--- test/round_test.rb
+++ test/round_test.rb
@@ -64,4 +64,16 @@ class RoundTest < Minitest::Test
     assert_instance_of Turn, new_turn
   end
 
+  def test_current_card_changes_to_next_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal card_2, round.current_card
+  end
+
 end
