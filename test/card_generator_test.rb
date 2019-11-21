01965c1026675236cd58c6ca19da7bcd1eb273e7
David Atkins <atki1080@gmail.com>
2019-11-21 12:53:53 -0700

    Modify card_generator_test to match specification

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -5,8 +5,10 @@ require './lib/card_generator'
 class CardGeneratorTest < Minitest::Test
 
   def test_it_exists
-    card_generator = CardGenerator.new
+    filename = 'cards.txt'
+    card_generator = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, card_generator
   end
+
 end
