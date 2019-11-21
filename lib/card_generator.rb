cc1f678bb310af00a5516ddfd86620f2e85c95ee
David Atkins <atki1080@gmail.com>
2019-11-21 12:54:38 -0700

    Add initialize instructions

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,21 @@
-
+require './lib/card'
 class CardGenerator
 
-  def initialize
+  attr_reader :cards
+
+  def initialize(filename)
+    @cards = []
+
+    file = File.open(filename)
+    file_data = file.readlines.map(&:chomp)
+    file.close
+
+    file_data.each do |line|
+      split_line = line.split(%r{,\s*})
+      card = Card.new(split_line[0],split_line[1],split_line[2])
+      @cards << card
+    end
+
   end
-  
+
 end
